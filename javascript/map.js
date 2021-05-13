9300f838fa2bfa1d1009a7110a09b75adefb7f93
mennovanderspank <mennovanderspank@gmail.com>
2021-05-13 12:44:48 +0200

    maxzoom weggehaald

--- javascript/map.js
+++ javascript/map.js
@@ -10,7 +10,6 @@ function initMap() {
         view: new ol.View({
             center: ol.proj.fromLonLat([4.313891500144687, 51.579039627392966]),
             zoom: 14,
-            maxZoom: 16.9
         }),
         interactions: ol.interaction.defaults({
             //doubleclickzoom staat uit om te voorkomen dat mobiele gebruikers inzoomen op de website ipv de kaart
